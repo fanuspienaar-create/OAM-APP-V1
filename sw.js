3c6398014ac5b2dbd7ae3d6c12ee9604790ab76c
fanuspienaar-create <fanus.pienaar@yahoo.com>
2026-09-09 20:57:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'oam-studio-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/oam-logo.png',
+  './assets/oam-icon.png',
+  './assets/oam-favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+  )).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
