4687b33cc18c4567686abb526796d93ecbd99331
fanuspienaar-create <fanus.pienaar@yahoo.com>
2026-09-09 22:10:40 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='oam-studio-v6';
-const APP=['./','./index.html','./manifest.webmanifest','./assets/oam-logo.png','./assets/oam-logo.webp','./assets/oam-icon.png','./assets/oam-icon.webp','./assets/oam-favicon.png','./assets/apple-touch-icon.png','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).catch(()=>{}))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
